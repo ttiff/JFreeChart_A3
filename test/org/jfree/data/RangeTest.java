eed739fb08e48037163bd0853d2bc13747e8c432
ttiff <90669090+ttiff@users.noreply.github.com>
2026-02-24 17:42:08 -0700

    add testConstructor_ExceptionMessage() to Range constructor method

--- test/org/jfree/data/RangeTest.java
+++ test/org/jfree/data/RangeTest.java
@@ -6,12 +6,17 @@ import org.junit.jupiter.api.Test;
 
 class RangeTest {
 	private Range exampleRange;
-	
 
 	@Test
 	void test() {
-		exampleRange = new Range(-1,1);
-		assertEquals(0, exampleRange.getCentralValue(),0.1d,"The central value of (-1,1) is 0");
+		exampleRange = new Range(-1, 1);
+		assertEquals(0, exampleRange.getCentralValue(), 0.1d, "The central value of (-1,1) is 0");
 	}
 
+	@Test
+	void testConstructor_ExceptionMessage() {
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> new Range(10.0, 5.0));
+		assertTrue(exception.getMessage().contains("require lower"));
+	}
+	
 }

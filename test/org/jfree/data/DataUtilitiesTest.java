e8316702a2c3745a7ec827e39713950c8ad4790e
Kim Linh Hong <lhong123@mtroyal.ca>
2026-02-27 20:53:40 -0700

    fix equal method +add test for intersect+  cummulative tests

--- test/org/jfree/data/DataUtilitiesTest.java
+++ test/org/jfree/data/DataUtilitiesTest.java
@@ -1,8 +1,7 @@
 package org.jfree.data;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals; 
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.*; 
 import org.junit.jupiter.api.BeforeEach; 
 
@@ -11,6 +10,8 @@ import org.junit.jupiter.api.Test;
 class DataUtilitiesTest {
 	
 	private Values2D value;
+	private static final double EPS = 0.0000001;
+
  	 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -29,6 +30,23 @@ class DataUtilitiesTest {
       	verify(value, times(3)).getValue(anyInt(), anyInt());
 
 	}
-	
+
+	@Test
+    void cumulative_basic() {
+        DefaultKeyedValues data = new DefaultKeyedValues();
+        data.addValue("A", 5);
+        data.addValue("B", 9);
+        data.addValue("C", 2);
+
+        KeyedValues result = DataUtilities.getCumulativePercentages(data);
+
+        assertEquals(1.0, result.getValue(2).doubleValue(), EPS);
+    }
+
+    @Test
+    void cumulative_nullInput() {
+        assertThrows(IllegalArgumentException.class,
+                () -> DataUtilities.getCumulativePercentages(null));
+    }
 
 }

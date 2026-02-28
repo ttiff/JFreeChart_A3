578c7bcac3956f2595a56970510d4744dc66cd5f
Smesa479 <112909100+Smesa479@users.noreply.github.com>
2026-02-27 23:54:06 -0700

    Add unit tests for Range.constrain method
    Added additional tests for the constrain method in Range class.

--- test/org/jfree/data/ConstrainTest.java
+++ test/org/jfree/data/ConstrainTest.java
@@ -35,5 +35,52 @@ class ConstrainTest {
 
         assertEquals(6.0, result, 0.00001);
     }
+    
+   // Additional Tests:
+    @Test
+    void constrain_atLowerBound_returnsLowerBound() {
+        Range range = new Range(2.0, 6.0);
+
+        double result = range.constrain(2.0);
+
+        assertEquals(2.0, result, 0.00001);
+    }
+
+    @Test
+    void constrain_atUpperBound_returnsUpperBound() {
+        Range range = new Range(2.0, 6.0);
+
+        double result = range.constrain(6.0);
+
+        assertEquals(6.0, result, 0.00001);
+    }
+
+    @Test
+    void constrain_withNegativeRange_inside() {
+        Range range = new Range(-10.0, -5.0);
+
+        double result = range.constrain(-7.0);
+
+        assertEquals(-7.0, result, 0.00001);
+    }
+
+    @Test
+    void constrain_withNegativeRange_above() {
+        Range range = new Range(-10.0, -5.0);
+
+        double result = range.constrain(0.0);
+
+        assertEquals(-5.0, result, 0.00001);
+    }
+
+    @Test
+    void constrain_withZeroRange() {
+        Range range = new Range(0.0, 0.0);
+
+        double result = range.constrain(5.0);
+
+        assertEquals(0.0, result, 0.00001);
+    }
+    
 
 }
